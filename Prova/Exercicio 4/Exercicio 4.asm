33e19664cd47fd18e170bf1664e6aa96f853dc0c
Scandiuzzii <gabrieltorrezan@gmail.com>
2020-05-23 15:18:16 -0300

    Projeto Final

---
+++ Prova/Exercicio 4/Exercicio 4.asm
@@ -0,0 +1,20 @@
+;---------------------------------------------------
+; Programa: Prova - Exercicio 4
+; Autor: Gabriel S Torrezan
+; Data: 18.05.2020
+;---------------------------------------------------
+
+A EQU 5
+B EQU 10
+C EQU 15
+
+ORG 0 
+LDA A
+SUB B
+LDA A
+STA C
+HLT
+
+LDA B
+STA C 
+HLT
